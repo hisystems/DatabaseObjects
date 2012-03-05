29c2f767772838ab5ce48e661c27296fd7d32d54
Toby Wicks <toby@hisystems.com.au>
2012-03-05 20:46:11 +1030

    Added ability to drop database views. Closes #4.
    Use the SQLDropView class to generate a DROP VIEW statement

---
+++ SQL/Views/SQLDropView.vb
@@ -0,0 +1,40 @@
+﻿' ___________________________________________________
+'
+'  © Hi-Integrity Systems 2012. All rights reserved.
+'  www.hisystems.com.au - Toby Wicks
+' ___________________________________________________
+'
+
+Option Strict On
+Option Explicit On
+
+Namespace SQL
+
+    Public Class SQLDropView
+        Inherits SQLStatement
+
+        Private pstrName As String
+
+        Public Sub New(ByVal strViewName As String)
+
+            If String.IsNullOrEmpty(strViewName) Then
+                Throw New ArgumentNullException()
+            End If
+
+            pstrName = strViewName
+
+        End Sub
+
+        Public Overrides ReadOnly Property SQL() As String
+            Get
+
+                Return _
+                    "DROP VIEW " & _
+                    SQLConvertIdentifierName(pstrName, Me.ConnectionType) 
+
+            End Get
+        End Property
+
+    End Class
+
+End Namespace
