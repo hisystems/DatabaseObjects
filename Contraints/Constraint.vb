cf7d095c2b391688720e461f5f657d39ba704000
Toby Wicks <toby@hisystems.com.au>
2012-03-10 14:52:47 +1030

    Added Constraints.Constraint that accepts a predicate as the constraint. Closes #14.

---
+++ Contraints/Constraint.vb
@@ -0,0 +1,39 @@
+﻿
+' ___________________________________________________
+'
+'  (c) Hi-Integrity Systems 2012. All rights reserved.
+'  www.hisystems.com.au - Toby Wicks
+' ___________________________________________________
+'
+
+Option Strict On
+Option Explicit On
+
+Namespace Constraints
+
+    ''' <summary>
+    ''' </summary>
+    Public Class Constraint(Of T)
+        Implements IConstraint(Of T)
+
+        Private pobjPredicate As Func(Of T, Boolean)
+
+        Public Sub New(ByVal objPredicate As Func(Of T, Boolean))
+
+            If objPredicate Is Nothing Then
+                Throw New ArgumentNullException
+            End If
+
+            pobjPredicate = objPredicate
+
+        End Sub
+
+        Private Function ValueSatisfiesConstraint(value As T) As Boolean Implements IConstraint(Of T).ValueSatisfiesConstraint
+
+            Return pobjPredicate.Invoke(value)
+
+        End Function
+
+    End Class
+
+End Namespace
