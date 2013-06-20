6f74656cf699da97a692aa5a547aa5ebf79a6168
Toby Wicks <toby@hisystems.com.au>
2013-06-20 20:40:06 +0930

    Version 5.1.0.1

--- Misc/AssemblyInfo.cs
+++ Misc/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.CompilerServices;
 [assembly:AssemblyCopyright("2010 Hi-Integrity Systems")]
 [assembly:AssemblyTrademark("")]
 [assembly:AssemblyCulture("")]
-[assembly:AssemblyVersion("5.1.0.0")]
+[assembly:AssemblyVersion("5.1.0.1")]
 [assembly:CLSCompliant(true)]
 
 #if DEBUG
