5c3571a351a861daac737c0c44a07e20733d1fc1
Toby Wicks <toby@hisystems.com.au>
2016-05-15 22:16:34 +0930

    Added SQLLiteralExpression so that untranslated SQL can be injected into an SQL statement.

---
+++ SQL/Expressions/SQLLiteralExpression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DatabaseObjects.SQL;
+using DatabaseObjects.SQL.Serializers;
+
+namespace DatabaseObjects
+{
+    public class SQLLiteralExpression : SQLExpression
+    {
+        private readonly string literalValue;
+
+        public SQLLiteralExpression(string literalValue)
+        {
+            this.literalValue = literalValue;
+        }
+
+        internal override string SQL(Serializer serializer)
+        {
+            return literalValue;
+        }
+    }
+}
